9f0f956e0282b67d609d05f944dd6782d8de9b73
yumirak <yumirashadow@gmail.com>
2023-03-26 10:42:09 +0700

    fix sound

--- code/cgame/cg_playerstate.c
+++ code/cgame/cg_playerstate.c
@@ -28,26 +28,23 @@ void CG_CheckAmmo( void ) {
 			continue;
 		}
 		switch ( i ) {
-		case WP_ROCKET_LAUNCHER:
-            total = cg.snap->ps.ammo[WP_ROCKET_LAUNCHER] * 1000;
-            break;
+        case WP_ROCKET_LAUNCHER:
 		case WP_GRENADE_LAUNCHER:
 		case WP_RAILGUN:
 		case WP_SHOTGUN:
 #ifdef MISSIONPACK
 		case WP_PROX_LAUNCHER:
 #endif
-            //total = cg.snap->ps.ammo[i] * 1000;
+            total += cg.snap->ps.ammo[i] * 1000;
 			break;
 		default:
-            //total = cg.snap->ps.ammo[i] * 200;
+            total += cg.snap->ps.ammo[i] * 200;
 			break;
 		}
         if ( total >= 5000 ) {
 			cg.lowAmmoWarning = 0;
 			return;
-		}
-
+        }
 	}
 
 	previous = cg.lowAmmoWarning;
@@ -60,7 +57,7 @@ void CG_CheckAmmo( void ) {
 
 	// play a sound on transitions
 	if ( cg.lowAmmoWarning != previous ) {
-		trap_S_StartLocalSound( cgs.media.noAmmoSound, CHAN_LOCAL_SOUND );
+        trap_S_StartLocalSound( cgs.media.noAmmoSound, CHAN_LOCAL_SOUND );
 	}
 }
 
